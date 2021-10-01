98c310190f5e11c8af251e0ae17adbc49be2a1f8
Yash <yash.dabhi@marutitech.com>
2021-10-01 12:52:21 +0530

    Template Tap-qualtrics

---
+++ setup.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+from setuptools import setup
+
+setup(
+    name="tap-qualtrics",
+    version="0.1.0",
+    description="Singer.io tap for extracting data",
+    author="Stitch",
+    url="http://singer.io",
+    classifiers=["Programming Language :: Python :: 3 :: Only"],
+    py_modules=["tap_qualtrics"],
+    install_requires=[
+        # NB: Pin these to a more specific version for tap reliability
+        "singer-python==5.12.2",
+        "requests",
+    ],
+    entry_points="""
+    [console_scripts]
+    tap-qualtrics=tap_qualtrics:main
+    """,
+    packages=["tap_qualtrics"],
+    package_data = {
+        "schemas": ["tap_qualtrics/schemas/*.json"]
+    },
+    include_package_data=True,
+)
